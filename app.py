e021fb5e14a25a64bd7e2bc5e716d2a164df3bed
Cemre Efe Karakas <cemrek@amazon.com>
2024-03-21 21:24:13 +0000

    Working searcher with options

--- app.py
+++ app.py
@@ -2,24 +2,37 @@ from flask import Flask, render_template, request
 import sqlite3
 from metaphone import doublemetaphone
 from nltk import edit_distance
+from eng_to_ipa import ipa_list
 
 app = Flask(__name__)
 
 # Function to retrieve top 10 similar names based on phonetic representation
-def get_similar_names(input_name):
+def get_similar_names(input_name, search_type):
     conn = sqlite3.connect('names_database.db')
     cursor = conn.cursor()
-    input_phonetic_repr = doublemetaphone(input_name)[0]
-    cursor.execute('''SELECT name FROM names''')
+    cursor.execute('''SELECT * FROM names''')
     all_names = cursor.fetchall()
     similar_names = []
 
-    for name in all_names:
-        name_phonetic_repr = doublemetaphone(name[0])[0]
-        similarity_score = edit_distance(input_phonetic_repr, name_phonetic_repr)
-        similar_names.append((name[0], name_phonetic_repr, similarity_score))
+    input_phonetic_repr = doublemetaphone(input_name)[0]
+    input_ipa = ipa_list(input_name)[0][0]
 
-    similar_names.sort(key=lambda x: x[1])  # Sort by similarity score
+    for name, gender, name_phonetic_repr, name_ipa, name_ipa_alts in all_names:
+        
+        if search_type == 'ipa':
+            similarity_score = edit_distance(input_name, name_ipa)
+        elif search_type == 'english-mp':
+            similarity_score = edit_distance(input_phonetic_repr, name_phonetic_repr)
+        elif search_type == 'english-ipa':
+            similarity_score = edit_distance(input_ipa, name_ipa)
+        elif search_type == 'english-mixedapproach':
+            similarity_score = edit_distance(input_ipa, name_phonetic_repr) * 100 + edit_distance(input_phonetic_repr, name_phonetic_repr)
+        elif search_type == 'mp':
+            similarity_score = edit_distance(input_name.upper(), name_phonetic_repr)
+
+        similar_names.append((name, gender, name_phonetic_repr, name_ipa, name_ipa_alts, similarity_score))
+
+    similar_names.sort(key=lambda x: x[-1])  # Sort by similarity score
     conn.close()
     return similar_names[:10]
 
@@ -30,7 +43,8 @@ def index():
 @app.route('/find_similar_names', methods=['POST'])
 def find_similar_names():
     input_name = request.form['name']
-    similar_names = get_similar_names(input_name)
+    search_type = request.form['search_type']  # Added line to retrieve search_type
+    similar_names = get_similar_names(input_name, search_type)  # Updated function call
     return render_template('results.html', input_name=input_name, similar_names=similar_names)
 
 if __name__ == '__main__':
--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,23 @@
 import csv
 import sqlite3
-from nltk import edit_distance
+from tqdm import tqdm
 from metaphone import doublemetaphone
+from eng_to_ipa import ipa_list
 
 # Function to calculate phonetic representation using Double Metaphone
 def calculate_phonetic_representation(name):
     return doublemetaphone(name)[0]
 
-# Function to create the SQLite database and populate it with name-gender-phonetic pairs
+# Function to calculate IPA transcription of a name
+def calculate_ipa_transcription(name):
+    ipa = ipa_list(name)[0]
+    if ipa:
+        ipa_transcription = ipa[0]
+        ipa_alternatives = ','.join(ipa[1:])
+        return ipa_transcription, ipa_alternatives
+    else:
+        return None, None
+
 def create_database(csv_file, db_file):
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
@@ -15,25 +25,31 @@ def create_database(csv_file, db_file):
                         name TEXT,
                         gender TEXT,
                         phonetic_representation TEXT,
+                        ipa_transcription TEXT,
+                        ipa_alternatives TEXT,
                         PRIMARY KEY (name, gender)
                     )''')
 
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
         next(reader)  # Skip header
-        for row in reader:
+        total_rows = sum(1 for row in file)
+        file.seek(0)  # Reset file pointer for iteration
+        for row in tqdm(reader, total=total_rows, desc="Processing CSV"):
             name = row[1]
             gender = row[3]
             phonetic_repr = calculate_phonetic_representation(name)
+            ipa_transcription, ipa_alternatives = calculate_ipa_transcription(name)
             try:
-                cursor.execute('''INSERT INTO names (name, gender, phonetic_representation)
-                                VALUES (?, ?, ?)''', (name, gender, phonetic_repr))
+                cursor.execute('''INSERT INTO names (name, gender, phonetic_representation, ipa_transcription, ipa_alternatives)
+                                VALUES (?, ?, ?, ?, ?)''', (name, gender, phonetic_repr, ipa_transcription, ipa_alternatives))
             except sqlite3.IntegrityError:
                 # Skip duplicate names with the same gender
                 pass
 
     conn.commit()
     conn.close()
+
 
 if __name__ == "__main__":
     csv_file = "names.csv"
